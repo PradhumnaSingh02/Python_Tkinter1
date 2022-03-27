9af03edda41c068feff671bdd0e0dca99bc5c390
Pradhumna Singh <89626211+PradhumnaSingh02@users.noreply.github.com>
2022-03-27 18:48:07 +0530

    Add files via upload

---
+++ gui0.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+pradhumna_root = Tk()
+
+pradhumna_root.geometry('733x44')
+
+statement = Label(text="This is my first GUI Program")
+statement.pack()
+pycharm = Label(text='Welcome to pycharm window')
+pycharm.pack()
+pradhumna_root.mainloop()
